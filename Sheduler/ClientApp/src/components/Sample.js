7509d55225ac88b60530b7c2e9be0fefb98df1cb
Asiri Mahendralal <asirim@abansgroup.com>
2019-06-18 17:41:48 +0530

    Add project files.

---
+++ Sheduler/ClientApp/src/components/Sample.js
@@ -0,0 +1,17 @@
+﻿
+import canvas from 'canvasjs.min.js';
+import React, { Component } from 'react';
+
+
+class Sample extends Component
+{
+    render() {
+        return (
+            <div>
+                <h1>Sample</h1>
+                <p>sample body</p>
+            </div>
+        );
+    }
+}
+export default Sample;
